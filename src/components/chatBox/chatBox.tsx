75d72c7cb0cebbbb92312377f021d4a49352658f
riddleydiddely <donny.ho@getriddle.com>
2023-07-31 12:28:51 +0200

    added bank balanes

--- src/components/chatBox/chatBox.tsx
+++ src/components/chatBox/chatBox.tsx
@@ -1,19 +1,20 @@
-import React, { FC, Fragment, ReactNode } from 'react';
+import React, {FC, Fragment, ReactNode} from 'react';
 
-import { TbHexagonLetterR } from "react-icons/tb";
-import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
-import { PropsWithChildren } from 'react'
-import { FiMenu } from "react-icons/fi";
-import { useState } from 'react';
+import {TbHexagonLetterR} from "react-icons/tb";
+import {AiOutlineDown, AiOutlineUp} from "react-icons/ai";
+import {PropsWithChildren} from 'react'
+import {FiMenu} from "react-icons/fi";
+import {useState} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useSession, signIn, signOut } from "next-auth/react"
-import { Session } from "next-auth";
+import {useSession, signIn, signOut} from "next-auth/react"
+import {Session} from "next-auth";
 
 
 type ChatBoxProps = {
     session: Session | null
 };
+
 function getCopilotResponse(message: string): JSX.Element {
     const timestamp: string = new Date().toDateString();
     return (
@@ -31,13 +32,14 @@ function getCopilotResponse(message: string): JSX.Element {
         </div>
     )
 }
+
 function getUserInput(message: string, image_source: string, name: string): JSX.Element {
     const timestamp: string = new Date().toDateString();
     return (
         <div className="chat chat-end">
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                    <img src={image_source} />
+                    <img src={image_source}/>
                 </div>
             </div>
             <div className="chat-header">
@@ -53,13 +55,14 @@ function getUserInput(message: string, image_source: string, name: string): JSX.
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 export default function ChatBox(props: ChatBoxProps) {
 
     const response1a = getCopilotResponse("Sure here is how Denmark performed last month")
     const image = props.session?.user?.image ?? ''
     const name = props.session?.user?.name ?? ''
 
-    const handleKeyDown = async (event:any) => {
+    const handleKeyDown = async (event: any) => {
         let tempConv = conversation;
         if (event.key === 'Enter') {
             await sleep(500)
@@ -76,7 +79,7 @@ export default function ChatBox(props: ChatBoxProps) {
     };
     let initialConversation: JSX.Element[] = [
         getCopilotResponse("How can I help you today?"),
-        
+
 
     ]
     const [chatOpen, setChatOpen] = useState(false)
@@ -87,7 +90,8 @@ export default function ChatBox(props: ChatBoxProps) {
         <div className="fixed bottom-0 right-0 z-50 shadow-xl pr-4 pb-4  ">
 
 
-            <div className={`w-80 ${chatOpen ? "h-96" : "h-10"} duration-700 flex flex-col rounded-lg border border-gray-300 shadow-md bg-white`}>
+            <div
+                className={`w-80 ${chatOpen ? "h-96" : "h-10"} duration-700 flex flex-col rounded-lg border border-gray-300 shadow-md bg-white`}>
 
                 <div className={`flex items-center justify-between border-b p-2 `}>
 
@@ -107,11 +111,12 @@ export default function ChatBox(props: ChatBoxProps) {
                 </div>
 
                 <div className={`flex items-center border-t p-2 duration-1000 ${chatOpen ? "" : "hidden"}`}>
-                    
-                        <input type="text" placeholder="Ask co-pilot anything" className="input-md input-bordered w-full max-w-xs" onKeyDown={(input) => handleKeyDown(input)} />
-                    
+
+                    <input type="text" placeholder="Ask co-pilot anything"
+                           className="input-md input-bordered w-full max-w-xs"
+                           onKeyDown={(input) => handleKeyDown(input)}/>
                     <div>
-                  
+
                     </div>
                 </div>
             </div>
